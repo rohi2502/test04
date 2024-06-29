12b759a2f29ae029593487931487cbd1bd21245b
rohi2502 <rohithagg2502@gmail.com>
2024-06-29 13:11:11 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+
+class Person
+{
+    public virtual void Greet()
+    {
+        Console.WriteLine("Hi! I am a person.");
+    }
+}
+
+class Employee : Person
+{
+    public override void Greet()
+    {
+        Console.WriteLine("Hello! I am an employee.");
+    }
+}
+class program
+{
+    static void Main()
+    {
+        Employee e1 = new Employee();
+        e1.Greet();
+    } 
+}
